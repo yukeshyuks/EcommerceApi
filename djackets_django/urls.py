d4bde7051fc4e671f64215470fa647eb246df3e3
Yukesh_yuks <yukeshyukss@gmail.com>
2022-02-24 11:06:50 +0530

    intial changes

--- djackets_django/urls.py
+++ djackets_django/urls.py
@@ -1,20 +1,19 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include,re_path
+from django.urls import path, include, re_path
 # drf_yasg code starts here
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.static import serve
+from django.conf.urls import url
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Flexamarket product API",
+        title="Ecommerce App",
         default_version='v1',
-        description="Welcome to the world of flexa",
-        terms_of_service="https://www.flexamarket.org",
-        contact=openapi.Contact(email="cto@flexamarket.com"),
-        license=openapi.License(name="Awesome IP"),
+        description="Welcome to Ecommerce api",
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -22,14 +21,18 @@ schema_view = get_schema_view(
 # ends here
 urlpatterns = [
     re_path(r'^doc(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),  #<-- Here
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),  # <-- Here
     path('doc/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),  #<-- Here
+         name='schema-swagger-ui'),  # <-- Here
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
-         name='schema-redoc'),  #<-- Here
+         name='schema-redoc'),  # <-- Here
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('product.urls')),
     path('api/v1/', include('order.urls')),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
